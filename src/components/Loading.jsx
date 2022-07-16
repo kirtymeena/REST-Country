6f312953f918b7a2bb2493bcbbb67da8fbbbf26f
Kirty Meena <kirtymeena@gmail.com>
2022-07-16 18:03:26 +0530

    created loading component

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,41 +3,15 @@ import React from "react";
 const Loading = () => {
   return (
     <div className="flex__wrapper">
-      <div className="jello-horizontal1">
-        <svg
-          width="2rem"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-        >
-          <path
-            fill="#0092E4"
-            d="M12,2A10,10,0,1,0,22,12,10,10,0,0,0,12,2Zm0,18a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z"
-          />
-        </svg>
-      </div>
-      <div className="jello-horizontal2">
-        <svg 
-          width="2rem"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-        >
-          <path
-            fill="#0092E4"
-            d="M12,2A10,10,0,1,0,22,12,10,10,0,0,0,12,2Zm0,18a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z"
-          />
-        </svg>
-      </div>
-      <div className="jello-horizontal3">
-        <svg
-          width="2rem"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-        >
-          <path
-            fill="#0092E4"
-            d="M12,2A10,10,0,1,0,22,12,10,10,0,0,0,12,2Zm0,18a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z"
-          />
-        </svg>
+      <div class="loading">
+        <div className="dot">L</div>
+        <div className="dot">O</div>
+        <div className="dot">A</div>
+        <div className="dot">D</div>
+        <div className="dot">I</div>
+        <div className="dot">N</div>
+        <div className="dot">G</div>
+        <span className="text">FETCHING COUNTRIES...</span>
       </div>
     </div>
   );
--- src/pages/CountryList.js
+++ src/pages/CountryList.js
@@ -15,103 +15,104 @@ const CountryList = () => {
   const region = useSelector((state) => state.countryReducer.region);
   const [searchTerm, setTerm] = useState("");
 
-
-  
   useEffect(() => {
-    const fetchData=()=>{ 
-        dispatch(fetchAll());
-    }
-    fetchData()
-    
-
+    const fetchData = () => {
+      dispatch(fetchAll());
+    };
+    console.log("loading", loading);
+    fetchData();
   }, [dispatch, theme, region]);
 
   const isLoading = () => {
-    return loading ? <Loading /> : "";
+    return <Loading /> 
   };
 
   return (
     <div className="container list__wrapper">
-      <div>
-        <div className="search__bar">
-          <div>
-            <svg
-              className="search__icon"
-              xmlns="http://www.w3.org/2000/svg"
-              width="1.8rem"
-              viewBox="0 0 24 24"
-            >
-              <path
-                fill={`${theme ? "#fafaf9" : "#080808"}`}
-                d="M21.71,20.29,18,16.61A9,9,0,1,0,16.61,18l3.68,3.68a1,1,0,0,0,1.42,0A1,1,0,0,0,21.71,20.29ZM11,18a7,7,0,1,1,7-7A7,7,0,0,1,11,18Z"
+      {loading ? (
+        isLoading()
+      ) : (
+        <div>
+          <div className="search__bar">
+            <div>
+              <svg
+                className="search__icon"
+                xmlns="http://www.w3.org/2000/svg"
+                width="1.8rem"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill={`${theme ? "#fafaf9" : "#080808"}`}
+                  d="M21.71,20.29,18,16.61A9,9,0,1,0,16.61,18l3.68,3.68a1,1,0,0,0,1.42,0A1,1,0,0,0,21.71,20.29ZM11,18a7,7,0,1,1,7-7A7,7,0,0,1,11,18Z"
+                />
+              </svg>
+
+              <input
+                className={`search ${theme ? "dark__input" : "light__input"}`}
+                type="text"
+                placeholder="Search for a country..."
+                value={searchTerm}
+                onChange={(e) => setTerm(e.target.value)}
               />
-            </svg>
-
-            <input
-              className={`search ${theme ? "dark__input" : "light__input"}`}
-              type="text"
-              placeholder="Search for a country..."
-              value={searchTerm}
-              onChange={(e) => setTerm(e.target.value)}
-            />
+            </div>
+            <div>
+              <Dropdown />
+            </div>
           </div>
-          <div>
-            <Dropdown />
+
+          <div className="country__list">
+            {region === ""
+              ? getData
+                  .filter((c) => {
+                    if (searchTerm === "") {
+                      return c;
+                    } else if (
+                      c.name.common
+                        .toLowerCase()
+                        .includes(searchTerm.toLowerCase())
+                    ) {
+                      return c;
+                    }
+                  })
+                  .map((c) => (
+                    <Link
+                      to={`/country/${c.name.common}`}
+                      className="link"
+                      key={c.altSpellings[0]}
+                    >
+                      <CardLayout
+                        name={c.name.common}
+                        flag={c.flags.svg}
+                        population={c.population}
+                        region={c.region}
+                        capital={c.capital}
+                      />
+                    </Link>
+                  ))
+              : getData
+                  .filter((d) => {
+                    if (d.region === region) {
+                      return d;
+                    }
+                  })
+                  .map((d) => (
+                    <Link
+                      to={`/country/${d.name.common}`}
+                      className="link"
+                      key={d.altSpellings[0]}
+                    >
+                      <CardLayout
+                        name={d.name.common}
+                        flag={d.flags.svg}
+                        population={d.population}
+                        region={d.region}
+                        capital={d.capital}
+                      />
+                    </Link>
+                  ))}
           </div>
         </div>
-
-        <div className="country__list">
-          {region===""
-            ? getData
-                .filter((c) => {
-                  if (searchTerm === "") {
-                    return c;
-                  } else if (
-                    c.name.common
-                      .toLowerCase()
-                      .includes(searchTerm.toLowerCase())
-                  ) {
-                    return c;
-                  }
-                })
-                .map((c) => (
-                  <Link
-                    to={`/country/${c.name.common}`}
-                    className="link"
-                    key={c.altSpellings[0]}
-                  >
-                    <CardLayout
-                      name={c.name.common}
-                      flag={c.flags.svg}
-                      population={c.population}
-                      region={c.region}
-                      capital={c.capital}
-                    />
-                  </Link>
-                ))
-            : getData
-                .filter((d) => {
-                  if (d.region === region) {
-                    return d;
-                  } 
-                })
-                .map((d) => (
-                  <Link
-                    to={`/country/${d.name.common}`}
-                    className="link"
-                    key={d.altSpellings[0]}
-                  >
-                    <CardLayout
-                      name={d.name.common}
-                      flag={d.flags.svg}
-                      population={d.population}
-                      region={d.region}
-                      capital={d.capital}
-                    />
-                  </Link>
-                ))}
-        </div>
-      </div>
+      )}
     </div>
   );
 };
